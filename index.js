61421ba55eb3ba7dc0f7ca205bf427e6df2f8bf8
Rafael Brigati <johndoe@example.comgit config --global user.name Rafael>
2023-05-18 16:36:35 +0200

    finished

--- index.js
+++ index.js
@@ -1,17 +1,77 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item)
+    this.length ++;
+    return this.items.sort((a, b) => a-b);
+  }
 
-  max() {}
+  get(pos) {
+    if (pos >= this.length){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
-  min() {}
+  max() {
+    if (this.length > 0){
+      return Math.max(...this.items);
+    }
+    else{
+      if (this.length ===0){
+        throw new Error('EmptySortedList');
+      } 
 
-  sum() {}
+    }
+    
+  }
 
-  avg() {}
+  min() {
+    if (this.length >0){
+      return Math.min(...this.items);
+    } else {
+      if (this.length === 0){
+        throw new Error('EmptySortedList');
+      }
+
+    }
+    
+  }
+
+  sum() {
+    if (this.items.length ===0){
+      throw new Error('EmptySortedList');
+    } else {
+      if (this.items ===[]){
+        return 0;
+      }
+    }
+    let total = 0;
+    this.items.forEach(element => {
+      total +=element;
+      
+    })
+    return total;
+    
+  }
+
+  avg() {
+    if (this.length > 0) {
+      let result = this.sum() / this.length;
+      return result;
+    } else {
+      if (this.items.length ===0){
+        throw new Error('EmptySortedList');
+      }
+
+    }
+    
+  }
 }
 
 module.exports = SortedList;
